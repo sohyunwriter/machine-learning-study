36b171344cf219f10f77f5f205415edb75ac2f59
SOHYEON <44013936+sohyunwriter@users.noreply.github.com>
2019-07-16 18:05:46 +0900

    Update logistic_regression_heart.py

--- study/logistic_regression_heart.py
+++ study/logistic_regression_heart.py
@@ -1,56 +1,60 @@
-import tensorflow as tf
+/**
+    ML STUDY 1(19.07.16) - logistic regression for heart dataset
+    @sohyunwriter (brightcattle@gmail.com)
+**/
+
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import MinMaxScaler
-
-tf.set_random_seed(777)
-
-## data preprocessing
-
-def data_clean(df):
-    df.head()
-    for feature_name in ['AHD', 'ChestPain', 'Thal']:
-        #pd.get_dummies(df[feature_name], prefix=feature_name, drop_first=True)
-        df = pd.concat([df.drop(feature_name, axis=1), pd.get_dummies(df[feature_name])], axis=1)
-
-    for feature_name in ['Age', 'RestBP', 'Chol', 'RestECG', 'MaxHR', 'Oldpeak', 'Slope','Ca']:
-        #std = np.std(df[feature_name])
-        #mean = np.mean(df[feature_name])
-        max = np.max(df[feature_name])
-        min = np.max(df[feature_name])
-        #df[feature_name] = (df[feature_name] - mean) / std
-        df[feature_name] = (df[feature_name] - min) / max - min
-        #mX = mean(df[feature_name], 1)
-        #print("000")
-
-    train_x = df.iloc[:, 0:-1]
-    #train_x = train_x.apply(lambda x: (x - np.mean(x)) / (np.max(x) - np.min(x)))
-    train_y = df.iloc[:, [-1]]
-    return train_x, train_y
+import matplotlib.pyplot as plt
+import tensorflow as tf
 
 df = pd.read_csv("./data/Heart_Train.csv")
-train_x, train_y = data_clean(df)
-#print(train_x, train_y)
-num_feature = len(train_x.columns)
+df.head()
+
+## 1. data preprocessing - categorical value --> numeric
+# creating dummy variable
+a = pd.get_dummies(df['ChestPain'], prefix = "cp")
+b = pd.get_dummies(df['Thal'], prefix = "thal")
+c = pd.get_dummies(df['Slope'], prefix = "slope")
+frames = [df, a, b, c] ## ???
+df = pd.concat(frames, axis=1)
+df.head()
+df = df.drop(columns = ['ChestPain', 'Thal', 'Slope'])
+df.ix[df.AHD == "No", "AHD"] = 0
+df.ix[df.AHD == "Yes", "AHD"] = 1
+y_train = df.iloc[:, [-1]] # y_train
+x_data = df.iloc[:, 0:-1]
+x_train = (x_data - np.min(x_data)) / (np.max(x_data) - np.min(x_data)).values  # normalize data (all columns of X) # x_train
 
 df = pd.read_csv("./data/Heart_Test.csv")
-test_x, test_y = data_clean(df)
+# creating dummy variable
+a = pd.get_dummies(df['ChestPain'], prefix = "cp")
+b = pd.get_dummies(df['Thal'], prefix = "thal")
+c = pd.get_dummies(df['Slope'], prefix = "slope")
+frames = [df, a, b, c]
+df = pd.concat(frames, axis=1)
+df.head()
+df = df.drop(columns = ['ChestPain', 'Thal', 'Slope'])
+df.ix[df.AHD == "No", "AHD"] = 0
+df.ix[df.AHD == "Yes", "AHD"] = 1
+y_test = df.iloc[:, [-1]] # y_test
+x_data = df.iloc[:, 0:-1]
 
+# normalize data
+x_test = (x_data - np.min(x_data)) / (np.max(x_data) - np.min(x_data)).values # x_test
+
+## 2. creating model for logistic regression using tensorflow
+num_feature = len(x_train.columns)
 X = tf.placeholder(tf.float32, shape=[None, num_feature])
 Y = tf.placeholder(tf.float32, shape=[None, 1])
 
-W = tf.Variable(tf.random_normal([num_feature, 1], mean= 0.01, stddev = 0.01), name='weight')
+W = tf.Variable(tf.random_normal([num_feature, 1], mean= 0.01, stddev = 0.01), name='weight') # xavier initialzation
 b = tf.Variable(tf.random_normal([1]), name='bias')
 
 # hypothesis using sigmoid
-eps = 1e-8
 hypothesis = tf.sigmoid(tf.matmul(X, W) + b)
-cost = tf.nn.sigmoid_cross_entropy_with_logits(logits=hypothesis, labels=Y)
-#cost = -tf.reduce_mean(Y*tf.log(hypothesis + eps) + (1-Y)*tf.log(1-hypothesis + eps))
-#cost = tf.reduce_mean(Y*tf.log(tf.clip_by_value(hypothesis, eps, 1.)) + (1 - Y) * tf.log(tf.clip_by_value(1-hypothesis, eps, 1.)))  # add eps to prevent divide by zero
-train = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(cost)
+cost = -tf.reduce_mean(Y*tf.log(hypothesis) + (1-Y)*tf.log(1-hypothesis)) # cross entropy
+train = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(cost)  # learning late = 0.001
 
 pred = tf.cast(hypothesis > 0.5, dtype=tf.float32)
 accuracy = tf.reduce_mean(tf.cast(tf.equal(pred, Y), dtype=tf.float32))
@@ -59,17 +63,124 @@ with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
     for step in range(10001):
-        sess.run(train, feed_dict={X: train_x, Y: train_y})
+        sess.run(train, feed_dict={X: x_train, Y: y_train})
         if step % 200 == 0:
-            print(step, sess.run(cost, feed_dict={X: train_x, Y: train_y}))
-            train_accuracy = sess.run(accuracy, feed_dict={X: train_x, Y: train_y})
-            test_accuracy = sess.run(accuracy, feed_dict={X: test_x, Y: test_y})
-            print("Train Accuracy : ", train_accuracy, "Test Accuracy : ", test_accuracy)
+            print(step, sess.run(cost, feed_dict={X: x_train, Y: y_train})) # print cost each 200 step
+            train_accuracy = sess.run(accuracy, feed_dict={X: x_train, Y: y_train})
+            test_accuracy = sess.run(accuracy, feed_dict={X: x_test, Y: y_test})
+            print("Train Accuracy : ", train_accuracy, "Test Accuracy : ", test_accuracy) # print train_accuracy, test_accuracy each 200 step
 
-    train_accuracy = sess.run(accuracy, feed_dict={X: train_x, Y: train_y})
-   # print("\nHypothesis: ", h, "\nCorrect (Y) ", c, "\nAccuracy: ", a)
-    test_accuracy = sess.run(accuracy, feed_dict={X: test_x, Y: test_y})
-    print("Train Accuracy : ", train_accuracy, "Test Accuracy : ", test_accuracy)
+    # print final train_accuracy, test_accuracy
+    train_accuracy = sess.run(accuracy, feed_dict={X: x_train, Y: y_train})
+    #print("\nHypothesis: ", h, "\nCorrect (Y) ", c, "\nAccuracy: ", a)
+    test_accuracy = sess.run(accuracy, feed_dict={X: x_test, Y: y_test})
+    print("Final Train Accuracy : ", train_accuracy, "Final Test Accuracy : ", test_accuracy)
+    
+    
+## result
+/**
+0 0.349779
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+200 0.32993463
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+400 0.3147886
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+600 0.30298212
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+800 0.29360494
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+1000 0.28603125
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+1200 0.27982104
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+1400 0.27465826
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+1600 0.27031162
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+1800 0.26660895
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+2000 0.2634202
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+2200 0.26064575
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+2400 0.2582082
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+2600 0.25604704
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+2800 0.2541142
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+3000 0.25237134
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+3200 0.2507876
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+3400 0.2493377
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+3600 0.24800117
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+3800 0.24676104
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+4000 0.2456034
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+4200 0.24451646
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+4400 0.24349052
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+4600 0.24251737
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+4800 0.24159004
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+5000 0.24070267
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+5200 0.2398503
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+5400 0.23902857
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+5600 0.2382339
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+5800 0.2374631
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+6000 0.23671347
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+6200 0.23598269
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+6400 0.23526876
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+6600 0.23456985
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+6800 0.23388448
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+7000 0.23321128
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+7200 0.23254907
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+7400 0.23189688
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+7600 0.23125379
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+7800 0.230619
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+8000 0.22999181
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+8200 0.22937162
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+8400 0.22875793
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+8600 0.22815016
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+8800 0.22754799
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+9000 0.22695099
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+9200 0.22635883
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+9400 0.22577122
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+9600 0.22518791
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+9800 0.22460864
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+10000 0.2240332
+Train Accuracy :  0.9276596 Test Accuracy :  0.9298246
+Final Train Accuracy :  0.9276596 Final Test Accuracy :  0.9298246
+**/
 
-
-    # Train Accuracy :  0.6255319 Test Accuracy :  0.5964912
